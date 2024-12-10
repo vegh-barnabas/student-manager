7e8c16da20b27d7578e4c738e1a10c927a97564f
vegh-barnabas <62122932+vegh-barnabas@users.noreply.github.com>
2024-12-10 19:43:46 +0100

    Student-list: put php block on top of the code

--- student-list.php
+++ student-list.php
@@ -1,4 +1,61 @@
-<?php session_start() ?>
+<?php
+    session_start();
+
+    require "helper-functions.php";
+
+    if(!isset($_SESSION["user"])) {
+        header("Location: $page");
+        exit();
+    }
+
+    $students = [
+        [
+            "id" => 1,
+            "name" => "Thommas Young",
+            "neptun" => "KMLT44",
+            "DOB" => "2005-04-30",
+            "gender" => "male",
+            "classes" => [
+                "Webprogramming",
+                "Discrete Mathematics",
+                "Linux Basics"
+            ]
+        ],
+        [
+            "id" => 2,
+            "name" => "Jim Old",
+            "neptun" => "ABC254",
+            "DOB" => "2000-04-27",
+            "gender" => "male",
+            "classes" => [
+                "Webprogramming",
+            ]
+        ],
+        [
+            "id" => 3,
+            "name" => "Greta Neither",
+            "neptun" => "LMO8MY",
+            "DOB" => "2003-02-07",
+            "gender" => "female",
+            "classes" => [
+                "Linux Basics",
+            ]
+        ],
+    ];
+
+    $oldest_student = null;
+    $oldest_timestamp = null;
+
+    // Get oldest student
+    foreach($students as $student) {
+        $current_timestamp = strtotime($student["DOB"]); // number of seconds since Jan 1 1970
+
+        if($oldest_timestamp == null || $current_timestamp < $oldest_timestamp) {
+            $oldest_timestamp = $current_timestamp;
+            $oldest_student = $student;
+        }
+    }
+?>
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -8,62 +65,6 @@
         <link rel="stylesheet" href="styles.css" />
     </head>
     <body>
-        <?php
-            require "helper-functions.php";
-
-            if(!isset($_SESSION["user"])) {
-                header("Location: $page");
-                exit();
-            }
-
-            $students = [
-                [
-                    "id" => 1,
-                    "name" => "Thommas Young",
-                    "neptun" => "KMLT44",
-                    "DOB" => "2005-04-30",
-                    "gender" => "male",
-                    "classes" => [
-                        "Webprogramming",
-                        "Discrete Mathematics",
-                        "Linux Basics"
-                    ]
-                ],
-                [
-                    "id" => 2,
-                    "name" => "Jim Old",
-                    "neptun" => "ABC254",
-                    "DOB" => "2000-04-27",
-                    "gender" => "male",
-                    "classes" => [
-                        "Webprogramming",
-                    ]
-                ],
-                [
-                    "id" => 3,
-                    "name" => "Greta Neither",
-                    "neptun" => "LMO8MY",
-                    "DOB" => "2003-02-07",
-                    "gender" => "female",
-                    "classes" => [
-                        "Linux Basics",
-                    ]
-                ],
-            ];
-
-            $oldest_student = null;
-            $oldest_timestamp = null;
-
-            // Get oldest student
-            foreach($students as $student) {
-                $current_timestamp = strtotime($student["DOB"]); // number of seconds since Jan 1 1970
-
-                if($oldest_timestamp == null || $current_timestamp < $oldest_timestamp) {
-                    $oldest_timestamp = $current_timestamp;
-                    $oldest_student = $student;
-                }
-            }
-        ?>
 
         <nav class="navbar">
             <div id="left">
