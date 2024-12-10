31d94dc297d555f4f2418a3d0307c4a6f08a1e18
vegh-barnabas <62122932+vegh-barnabas@users.noreply.github.com>
2024-12-10 19:56:53 +0100

    Student-list: fix redirection, use DB students instead of mock

--- student-list.php
+++ student-list.php
@@ -2,53 +2,21 @@
     session_start();
 
     require "helper-functions.php";
+    require "storage.php";
 
     if(!isset($_SESSION["user"])) {
-        header("Location: $page");
-        exit();
+        redirect("index.php");
     }
 
-    $students = [
-        [
-            "id" => 1,
-            "name" => "Thommas Young",
-            "neptun" => "KMLT44",
-            "DOB" => "2005-04-30",
-            "gender" => "male",
-            "classes" => [
-                "Webprogramming",
-                "Discrete Mathematics",
-                "Linux Basics"
-            ]
-        ],
-        [
-            "id" => 2,
-            "name" => "Jim Old",
-            "neptun" => "ABC254",
-            "DOB" => "2000-04-27",
-            "gender" => "male",
-            "classes" => [
-                "Webprogramming",
-            ]
-        ],
-        [
-            "id" => 3,
-            "name" => "Greta Neither",
-            "neptun" => "LMO8MY",
-            "DOB" => "2003-02-07",
-            "gender" => "female",
-            "classes" => [
-                "Linux Basics",
-            ]
-        ],
-    ];
+    $user_storage = new Storage(new JsonIO("users.json"));
+    $students = $user_storage->findAll();
 
     $oldest_student = null;
     $oldest_timestamp = null;
 
     // Get oldest student
     foreach($students as $student) {
-        $current_timestamp = strtotime($student["DOB"]); // number of seconds since Jan 1 1970
+        $current_timestamp = strtotime($student["dateOfBirth"]); // number of seconds since Jan 1 1970
 
         if($oldest_timestamp == null || $current_timestamp < $oldest_timestamp) {
             $oldest_timestamp = $current_timestamp;
@@ -97,9 +65,9 @@
                     <?php foreach($students as $student): ?>
                         <tr>
                             <td><?= $student["id"] ?></td>
-                            <td><?= $student["name"] ?></td>
+                            <td><?= $student["username"] ?></td>
                             <td><?= $student["neptun"] ?></td>
-                            <td><?= $student["DOB"] ?></td>
+                            <td><?= $student["dateOfBirth"] ?></td>
                             <td><?= ucfirst($student["gender"]) ?></td>
                             <td>
                                 <ul>
@@ -114,7 +82,7 @@
             </table>
             <div class="statistics">
                 <p><strong>Statistics:</strong></p>
-                <p>Oldest Student:  <?= $oldest_student["name"] ?> with DOB <?= $oldest_student["DOB"] ?> (<?= calculate_age_from_DOB($oldest_student["DOB"]) ?> years old)</p>
+                <p>Oldest Student:  <?= $oldest_student["username"] ?> with DOB <?= $oldest_student["dateOfBirth"] ?> (<?= calculate_age_from_DOB($oldest_student["dateOfBirth"]) ?> years old)</p>
                 <p>Total Students: <?= count($students) ?></p>
             </div>
         </div>
